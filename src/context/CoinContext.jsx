9bc8ccd7927a801747c7cf8ae9a8fab242f8aebf
saksham365 <sakshamextraone@gmail.com>
2024-09-25 12:41:04 +0530

    Update CoinContext.jsx

--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -2,42 +2,59 @@ import { createContext, useEffect, useState } from "react";
 
 export const CoinContext = createContext();
 
-const CoinContextProvider = (props)=>{
+const CoinContextProvider = (props) => {
+  const API_KEY = "CG-AnAAxVvq1JrdTrybwprhcwCr";
 
-    const API_KEY = 'CG-AnAAxVvq1JrdTrybwprhcwCr'
+  const [allCoin, setAllCoin] = useState([]);
+  const [currency, setCurrency] = useState({
+    name: "usd",
+    symbol: "$",
+  });
 
-    const [allCoin, setAllCoin] = useState([]);
-    const [currency, setCurrency] = useState({
-        name: "usd",
-        symbol: "$"
-    })
+  const fetchAllCoin = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": API_KEY,
+      },
+    };
 
-    const fetchAllCoin = async ()=>{
-        const options = {
-            method: 'GET',
-            headers: {accept: 'application/json', 'x-cg-demo-api-key': API_KEY}
-          };
-          
-          fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options)
-            .then(response => response.json())
-            .then(response => setAllCoin(response))
-            .catch(err => console.error(err));
+    try {
+      const response = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}&order=market_cap_desc&per_page=50&page=1`, 
+        options
+      );
+      const data = await response.json();
+      setAllCoin(data);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-
-useEffect(()=>{
+  useEffect(() => {
     fetchAllCoin();
-},[currency])
 
-    const contextValue = {
-        allCoin, currency, setCurrency,API_KEY
-    }
+    const interval = setInterval(() => {
+      fetchAllCoin();
+    }, 30000);
 
-    return (
-        <CoinContext.Provider value={contextValue}>
-            {props.children}
-        </CoinContext.Provider>
-    )
-}
+    return () => clearInterval(interval);
+  }, [currency]);
+
+  const contextValue = {
+    allCoin,
+    currency,
+    setCurrency,
+    API_KEY,
+  };
+
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+};
 
 export default CoinContextProvider;
+
